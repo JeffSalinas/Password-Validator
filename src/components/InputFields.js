d3afcad8ad9d7ba3e5e240d05e5e03e6b0c01dd2
Jeffrey Salinas <jeff.salinas.music@gmail.com>
2020-01-23 00:33:23 -0600

    Add request to email api and email validation

--- src/components/InputFields.js
+++ src/components/InputFields.js
@@ -5,27 +5,27 @@ export default function InputFields ({ password, setPassword }) {
 
   return (
     <>
-    <p id="password-title">Password</p>
-    <div id="input-container">
-      <input
-        id="password-input"
-        aria-label="Insert Password"
-        aria-required="true"
-        type={isShown ? 'text' : 'password'}
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <div id="toggle-container">
+      <p id="password-title">Password</p>
+      <div id="input-container">
         <input
-          id="password-toggle"
-          aria-label="Show Password"
-          type="checkbox"
-          checked={isShown}
-          onChange={() => setIsShown(!isShown)}
+          id="password-input"
+          aria-label="Insert Password"
+          aria-required="true"
+          type={isShown ? 'text' : 'password'}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <p id="toggle-title">Show</p>
+        <div id="toggle-container">
+          <input
+            id="password-toggle"
+            aria-label="Show Password"
+            type="checkbox"
+            checked={isShown}
+            onChange={() => setIsShown(!isShown)}
+          />
+          <p id="toggle-title">Show</p>
+        </div>
       </div>
-    </div>
     </>
   )
 }
