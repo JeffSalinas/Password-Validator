2106c90391f907e34e9f8b63a722b519f4d20699
Jeffrey Salinas <jeff.salinas.music@gmail.com>
2020-01-22 21:04:44 -0600

    Add tests for input and toggle switch

--- cypress/integration/validation.js
+++ cypress/integration/validation.js
@@ -2,4 +2,18 @@ describe('Validator Tests', () => {
   it('validation page should exist', () => {
     cy.visit('/')
   })
+
+  it('should have an input field', () => {
+    cy.get('#password-input').should('exist')
+  })
+
+  it('should have a show/hide password toggle switch', () => {
+    cy.get('#password-toggle').should('exist')
+  })
+
+  it('should have toggle switch enabled', () => {
+    cy.get('#password-toggle').click().then(() => {
+      cy.get('#password-toggle').check().should('be.checked')
+    })
+  })
 })
