754b79a4974db5dc2fd9511382e9857a41263b2f
Jeffrey Salinas <jeff.salinas.music@gmail.com>
2020-01-22 21:58:33 -0600

    Render input and toggle button

--- src/components/Validator.js
+++ src/components/Validator.js
@@ -3,8 +3,25 @@ import React from 'react';
 export default function Validator () {
 
   return (
-    <div id="CSSTest">
-      hello world
+    <div>
+      <p id="password-title">Password</p>
+      <div id="input-container">
+        <input
+          id="password-input"
+          aria-label="Insert Password"
+          aria-required="true"
+          type="text"
+        />
+        <div id="toggle-container">
+          <input
+            id="password-toggle"
+            aria-label="Show Password"
+            type="checkbox"
+            checked={true}
+          />
+          <p id="toggle-title">Show</p>
+        </div>
+      </div>
     </div>
   )
 }
