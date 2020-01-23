3f83f4c8a22742cd89dbb2a27d8261c51f1c5a08
Jeffrey Salinas <jeff.salinas.music@gmail.com>
2020-01-22 23:01:01 -0600

    Move input fields to separate componenet

--- src/components/Validator.js
+++ src/components/Validator.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import InputFields from './InputFields';
 
 export default function Validator () {
   const [ password, setPassword ] = useState('');
-  const [ isShown, setIsShown ] = useState(false);
   const [ charCount, setCharCount ] = useState(false);
 
   useEffect(() => {
@@ -21,30 +21,9 @@ export default function Validator () {
     }
   }
 
-
   return (
     <div>
-      <p id="password-title">Password</p>
-      <div id="input-container">
-        <input
-          id="password-input"
-          aria-label="Insert Password"
-          aria-required="true"
-          type={isShown ? 'text' : 'password'}
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <div id="toggle-container">
-          <input
-            id="password-toggle"
-            aria-label="Show Password"
-            type="checkbox"
-            checked={isShown}
-            onChange={() => setIsShown(!isShown)}
-          />
-          <p id="toggle-title">Show</p>
-        </div>
-      </div>
+      <InputFields password={password} setPassword={setPassword}/>
       <ul>
         <li id="test-character-count" className={charCount ? "line-through" : "no-line"}>8-72 Characters</li>
         <li></li>
