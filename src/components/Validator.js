62389010bff5d28d073e170dade604674e3aa85d
Jeffrey Salinas <jeff.salinas.music@gmail.com>
2020-01-22 22:55:11 -0600

    Add character length validation

--- src/components/Validator.js
+++ src/components/Validator.js
@@ -1,8 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Validator () {
   const [ password, setPassword ] = useState('');
   const [ isShown, setIsShown ] = useState(false);
+  const [ charCount, setCharCount ] = useState(false);
+
+  useEffect(() => {
+    //get email
+  }, []);
+
+  useEffect(() => {
+    validateCharCount();
+  }, [password]);
+
+  function validateCharCount () {
+    if (password.length >= 8 && password.length <= 72) {
+      setCharCount(true);
+    } else {
+      setCharCount(false);
+    }
+  }
+
 
   return (
     <div>
@@ -27,6 +45,13 @@ export default function Validator () {
           <p id="toggle-title">Show</p>
         </div>
       </div>
+      <ul>
+        <li id="test-character-count" className={charCount ? "line-through" : "no-line"}>8-72 Characters</li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   )
 }
